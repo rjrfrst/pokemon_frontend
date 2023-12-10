471d56d3cb6b6d8e745c495c20f04140321339ed
Rayster Ramos <rjrfrst@gmail.com>
2023-12-10 15:46:46 +0000

    Pokemon list file created, imported and displays in the react app

---
+++ src/components/PokemonList.js
@@ -0,0 +1,9 @@
+const PokemonList = () => {
+    return (
+        <>
+            <h2>This is the list of pokemons</h2>
+        </>
+    );
+}
+ 
+export default PokemonList;
--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,7 +1,9 @@
+import PokemonList from "../components/PokemonList";
 const PokemonContainer = () => {
     return ( 
         <>
         <h1>This is the pokemon container</h1>
+        <PokemonList />
         </>
     );
 }
